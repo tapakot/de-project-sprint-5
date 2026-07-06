f6d3bb34f9e070e72dae115d5ccc6952e51b0832
tapakot <andgurevich@gmail.com>
2026-07-06 17:44:17 +0300

    sprint5

--- venv/lib/python3.12/site-packages/airflow/ui/dist/assets/editor.worker-gqzZP_00.js
+++
@@ -1,1 +0,0 @@
-var e=``+new URL(`editor.worker-CkVRLSnK.js`,import.meta.url).href;export{e as default};
--- venv/lib/python3.12/site-packages/airflow/ui/dist/assets/json.worker-DSuWmX2-.js
+++
@@ -1,1 +0,0 @@
-var e=``+new URL(`json.worker-wpSRThcD.js`,import.meta.url).href;export{e as default};
